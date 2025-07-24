1d0481cbc4ed1e00410a30dad583bad2462e6145
rmitos <mobileapps@rmitsolutions.net>
2025-07-24 15:52:06 +0530

    Modified distance filter in location settings.

---
+++ example/lib/camera_map.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:map_camera_flutter/map_camera_flutter.dart';
+
+class CameraMapPage extends StatelessWidget {
+  const CameraMapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Camera Map"),
+        ),
+        body: MapCameraLocation(
+          onImageCaptured: (ImageAndLocationData data) {
+            print('Captured image path: ${data.image?.length}');
+            print('Latitude: ${data.latitude}');
+            print('Longitude: ${data.longitude}');
+            //print('Location name: ${data.locationName}');
+            //print('Sublocation: ${data.subLocation}');
+          },
+        ));
+  }
+}
